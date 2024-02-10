65d68894071e72b7dfe2b07424c834fee761846f
Steffi Joji <steffijoji@gmail.com>
2024-02-10 19:36:39 -0300

    add initial settings

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login",
+    baseUrl: "https://opensource-demo.orangehrmlive.com",
     defaultCommandTimeout: 7000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
