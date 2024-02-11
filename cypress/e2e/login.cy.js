25adb7bc318a234e387731be6d0433861acaa17c
Steffi Joji <steffijoji@gmail.com>
2024-02-10 21:11:12 -0300

    add login tests

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,17 +2,60 @@
 
 import login_page from "../support/pages/login_page";
 
+const login_data = require('../fixtures/login_data.json')
+
 describe('Login', () => {
 
   beforeEach('Access Login Page', () => {
     login_page.accessLoginPage()
   })
 
-  it.only('Login without username and password', () => {
+  it('Login without credentials', () => {
     login_page.checkUsernameField()
     login_page.checkPasswordField()
     login_page.clickLoginButton()
     login_page.checkRequiredMessage('RequiredRequired')
   })
-  
+
+  it('Login without username', () => {
+    login_page.checkUsernameField()
+    login_page.fillPassword(login_data.valid_password)
+    login_page.clickLoginButton()
+    login_page.checkRequiredMessage('Required')
+  })
+
+  it('Login without password', () => {
+    login_page.fillUsername(login_data.valid_username)
+    login_page.checkPasswordField()
+    login_page.clickLoginButton()
+    login_page.checkRequiredMessage('Required')
+  })
+
+  it('Login with invalid credentials', () => {
+    login_page.fillUsername(login_data.invalid_username)
+    login_page.fillPassword(login_data.invalid_password)
+    login_page.clickLoginButton()
+    login_page.checkCredentialsMessage()
+  })
+
+  it('Login with invalid username', () => {
+    login_page.fillUsername(login_data.invalid_username)
+    login_page.fillPassword(login_data.valid_password)
+    login_page.clickLoginButton()
+    login_page.checkCredentialsMessage()
+  })
+
+  it('Login with invalid password', () => {
+    login_page.fillUsername(login_data.valid_username)
+    login_page.fillPassword(login_data.invalid_password)
+    login_page.clickLoginButton()
+    login_page.checkCredentialsMessage()
+  })
+
+  it('Login with valid credentials', () => {
+    login_page.fillUsername(login_data.valid_username)
+    login_page.fillPassword(login_data.valid_password)
+    login_page.clickLoginButton()
+    login_page.accessSecretPage()
+  })
 })
--- cypress/support/pages/login_page.js
+++ cypress/support/pages/login_page.js
@@ -4,7 +4,8 @@ const elements = {
     usernameField: '[name="username"]',
     passwordField: '[name="password"]',
     loginButton: '.orangehrm-login-button',
-    requiredMessage: '.oxd-input-field-error-message'
+    requiredMessage: '.oxd-input-field-error-message',
+    credentialsMessage: '.oxd-alert-content-text'
 }
 
 export default {
@@ -38,5 +39,28 @@ export default {
         cy.get(elements.requiredMessage)
             .should('be.visible')
             .should('have.text', requiredText)
+    },
+
+    fillPassword(password) {
+        cy.get(elements.passwordField)
+            .should('be.visible')
+            .type(password)
+    },
+
+    fillUsername(username) {
+        cy.get(elements.usernameField)
+            .should('be.visible')
+            .type(username)
+    },
+
+    checkCredentialsMessage() {
+        cy.get(elements.credentialsMessage)
+            .should('be.visible')
+            .should('have.text', 'Invalid credentials')
+    },
+
+    accessSecretPage() {
+        cy.get('.oxd-userdropdown-name')
+            .should('be.visible')
     }
 }
