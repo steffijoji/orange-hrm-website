fe458b79adc4366134eb706343b019e26a156cfb
Steffi Joji <steffijoji@gmail.com>
2024-02-14 08:58:13 -0300

    add personal details update tests

--- cypress/e2e/my_info.cy.js
+++ cypress/e2e/my_info.cy.js
@@ -1,1 +1,25 @@
 /// <reference types="cypress" />
+
+import login_page from "../support/pages/login_page";
+import my_info_page from "../support/pages/my_info_page";
+import { faker } from '@faker-js/faker';
+
+const login_data = require('../fixtures/login_data.json')
+
+describe('My Info', () => {
+
+    beforeEach('Access My Info Page', () => {
+        login_page.accessLoginPage()
+        login_page.fillUsername(login_data.valid_username)
+        login_page.fillPassword(login_data.valid_password)
+        login_page.clickLoginButton()
+        login_page.accessSecretPage()
+        my_info_page.accessMyInfoPage()
+    })
+
+    it.only('Update Personal Details', () => {
+        my_info_page.fillFirstName(faker.person.firstName())
+        my_info_page.fillMiddleName(faker.person.middleName())
+        my_info_page.fillLastName(faker.person.lastName())
+    })
+})
--- cypress/support/pages/my_info_page.js
+++ cypress/support/pages/my_info_page.js
@@ -1,1 +1,40 @@
 /// <reference types="cypress" />
+
+const elements = {
+    myInfoMenu: '[href$="/viewMyDetails"]',
+    firstName: '[name="firstName"]',
+    middleName: '[name="middleName"]',
+    lastName: '[name="lastName"]'
+
+}
+
+export default {
+    accessMyInfoPage() {
+        cy.get(elements.myInfoMenu)
+            .should('be.visible')
+            .click()
+    },
+
+    fillFirstName(firstName) {
+        cy.get(elements.firstName)
+            .should('be.visible')
+            .clear()
+            .type(firstName)
+    },
+
+    fillMiddleName(middleName) {
+        cy.get(elements.middleName)
+            .should('be.visible')
+            .clear()
+            .type(middleName)
+    },
+
+    fillLastName(lastName) {
+        cy.get(elements.lastName)
+            .should('be.visible')
+            .clear()
+            .type(lastName)
+    },
+}
+
+
